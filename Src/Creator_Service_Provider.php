26085939348c6ccc7286ddfedb950f7887b29e8a
Mai ZhiKun <maizk@haplox.com>
2016-09-04 12:39:48 +0800

    add service providerr

---
+++ Src/Creator_Service_Provider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace K_Laravel_Creator;
+
+use Illuminate\Support\ServiceProvider;
+use K_Laravel_Creator\Console\Commands\K_Make_Command;
+use K_Laravel_Creator\Console\Commands\K_Make_Controller;
+use K_Laravel_Creator\Console\Commands\K_Make_Factory;
+use K_Laravel_Creator\Console\Commands\K_Make_Migration;
+use K_Laravel_Creator\Console\Commands\K_Make_Model;
+use K_Laravel_Creator\Console\Commands\K_Make_Seeder;
+
+
+class Creator_Service_Provider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    protected $commands = [
+        'K_Make_Command' => 'command.command.k_creator',
+        'K_Make_Controller' => 'command.controller.k_creator',
+        'K_Make_Factory' => 'command.factory.k_creator',
+        'K_Make_Migration' => 'command.migration.k_creator',
+        'K_Make_Model' => 'command.model.k_creator',
+        'K_Make_Seeder' => 'command.seeder.k_creator',
+
+    ];
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        $this->registerCommands();
+    }
+
+    /**
+     * Register the cache related console commands.
+     *
+     * @return void
+     */
+    public function registerCommands()
+    {
+        foreach ($this->commands as $key => $value) {
+            $this->app->singleton($value, function ($app) {
+                return new $key;
+            });
+
+            $this->commands($value);
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'cache', 'cache.store', 'memcached.connector', 'command.cache.clear',
+        ];
+    }
+}
