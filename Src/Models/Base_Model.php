32e42899f0237e5ecd4fd9054dea78b43bee377e
Mai ZhiKun <maizk@haplox.com>
2016-09-04 23:34:53 +0800

    add model

---
+++ Src/Models/Base_Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace K_Laravel_Creator\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Base_Model extends Model implements Jsonable{
+
+    use SoftDeletes;
+
+
+
+}
+
